88478890af7150e2c61834ac887d4e2270c4ebdb
Tia Lendor <TiaLendon@pursuit.org>
2019-12-19 12:13:18 -0500

    adding firebase models and fuctions

---
+++ Seasonality/Seasonality/Posts.swift
@@ -0,0 +1,43 @@
+//
+//  Posts.swift
+//  Seasonality
+//
+//  Created by Tia Lendor on 12/19/19.
+//  Copyright © 2019 Jack Wong. All rights reserved.
+//
+
+import Foundation
+
+struct Post {
+    let title: String
+    let body: String
+    let id: String
+    let creatorID: String
+    
+    init(title: String, body: String, creatorID: String) {
+        self.title = title
+        self.body = body
+        self.creatorID = creatorID
+        self.id = UUID().description
+    }
+    
+    init?(from dict: [String: Any], id: String) {
+        guard let title = dict["title"] as? String,
+            let body = dict["body"] as? String,
+            let userID = dict["creatorID"] as? String else {
+                return nil
+        }
+        self.title = title
+        self.body = body
+        self.creatorID = userID
+        self.id = id
+    }
+    
+    var fieldsDict: [String: Any] {
+        return [
+            "title": self.title,
+            "body": self.body,
+            "creatorID": self.creatorID
+        ]
+    }
+}
